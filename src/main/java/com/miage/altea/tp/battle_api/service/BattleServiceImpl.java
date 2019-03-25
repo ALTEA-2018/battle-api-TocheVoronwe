a4506d178445ad0cb679a674bfab2ee4a06d4116
toche <christophe.maingard@epitech.eu>
2019-03-25 11:55:20 +0100

    feat(battle): first battle api

--- src/main/java/com/miage/altea/tp/battle_api/service/BattleServiceImpl.java
+++ src/main/java/com/miage/altea/tp/battle_api/service/BattleServiceImpl.java
@@ -1,17 +1,22 @@
 package com.miage.altea.tp.battle_api.service;
 
 import com.miage.altea.tp.battle_api.bo.Battle;
+import com.miage.altea.tp.battle_api.bo.Trainer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class BattleServiceImpl implements BattleService {
     @Value("${trainer-api.url}")
     String trainerApiUrl;
 
-    public Battle createBattle(String trainer, String opponent) {
+    public Battle createBattle(String trainerName, String opponentName) {
+
+        UUID uuid = UUID.randomUUID();
+
 
         return null;
     }
